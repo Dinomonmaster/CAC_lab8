4f0e23d8c85d609c4d8c14f84fb7a983b41f26b8
Dinomonmaster <CesarCarballo1993@gmail.com>
2024-04-12 17:16:19 -0400

    Menu/Encoder
    Adding encoder and main menu

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 """
-Cesar Carballo initial commit testing
+Team Members: Cesar Carballo, Jinghan Wu
 """
 
 def encode(password):
-    pass
+    encoded_password = ""
+    for char in password:
+        char = int(char)
+        char += 3
+        encoded_password += str(char)
+    return encoded_password
 
 if __name__ == "__main__":
     while True:
-        pass
+        print("Password Changer Menu Options:")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Exit")
+        print(" ")
+        choice = input("Please Select a Menu Option: ")
+        print(" ")
+        if (choice != "1") and (choice != "2") and (choice != "3"):
+            print("Invalid Menu option Please select again: ")
+            choice = input("Please Select a Menu Option: ")
+        if choice == "1":
+            password = input("Please Enter a Numeric Password to Encode: ")
+            password = encode(password)
+            print("Encoded Password is: ", password)
+            print(" ")
+
+        if choice == "3":
+            print("Thank you for USing Password Changer")
+            break
+
+
 
